4db85132eeffc7dfbdfcd7f754fcc2af9f2aaa6e
dzmitry.dzianisau@gmail.com <Dream-By>
2020-03-30 10:18:58 +0300

    Update project

--- app/src/main/java/by/dream/screensettings/MainActivity.java
+++ app/src/main/java/by/dream/screensettings/MainActivity.java
@@ -1,6 +1,7 @@
 package by.dream.screensettings;
 
 import android.graphics.Point;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
 import android.widget.TextView;
@@ -29,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
 
         TextView infotextview = (TextView) findViewById(R.id.textViewInfo);
         infotextview.setText(info);
+    }
+
+    public void OnDisplay(View view) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        String strScreen = "";
+        strScreen += "Width: " + String.valueOf(metrics.widthPixels) + " pixels"
+                + "\n";
+        strScreen += "Height: " + String.valueOf(metrics.heightPixels) + " pixels"
+                + "\n";
+        strScreen += "The Logical Density: " + String.valueOf(metrics.density)
+                + "\n";
+        strScreen += "X Dimension: " + String.valueOf(metrics.xdpi) + " dot/inch"
+                + "\n";
+        strScreen += "Y Dimension: " + String.valueOf(metrics.ydpi) + " dot/inch"
+                + "\n";
+        strScreen += "The screen density expressed as dots-per-inch: "
+                + metrics.densityDpi + "\n";
+        strScreen += "A scaling factor for fonts displayed on the display: "
+                + metrics.scaledDensity + "\n";
+
+        TextView infoTextView = findViewById(R.id.textView);
+        infoTextView.setText(strScreen);
     }
 }
